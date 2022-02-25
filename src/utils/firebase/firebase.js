4b416e98d21f18089cb2c3c1ea434e4e05920b11
Pranav Yeole <83200717+pranvinit@users.noreply.github.com>
2022-02-25 21:44:53 +0530

    Update firebase.js

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6E3FyKrj1n7Xfb37tfh3cUGNDqKiVtuo",
-  authDomain: "foodiespot-2766d.firebaseapp.com",
-  projectId: "foodiespot-2766d",
-  storageBucket: "foodiespot-2766d.appspot.com",
-  messagingSenderId: "23175827044",
-  appId: "1:23175827044:web:e4008a9d5b243d5e691971",
+  apiKey: "don't steal my api key please lol",
+  authDomain: "yourmom.app",
+  projectId: "urmom-2766d",
+  storageBucket: "urmom-69420.app.com",
+  messagingSenderId: "nah",
+  appId: "1:urmom:web:notfound",
 };
 
 initializeApp(firebaseConfig);
